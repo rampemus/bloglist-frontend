8aca093a07bf191ac134c3b0f6c73da85ce98cf5
rampemus <pasi.toivanen@live.com>
2019-09-02 14:13:29 +0300

    Login with feedback, saving token to state

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import loginService from './services/login'
 
 //TODO: after login show blogs
 //TODO: logging permanent and logout option
@@ -6,16 +7,31 @@ import React, { useState } from 'react'
 
 function App() {
     const [username, setUsername] = useState('')
-
     const [password, setPassword] = useState('')
+    const [user, setUser] = useState(null)
+    const [notification, setNotification] = useState({ message: 'no notifications', error:false })
 
-    const handleLogin = (event) => {
+    const handleLogin = async(event) => {
         event.preventDefault()
         console.log('logging event',username, password)
+
+        try {
+            const userData = await loginService.login(username,password)
+            setUser(userData)
+            setUsername('')
+            setPassword('')
+            setNotification({ message: 'logged in succesfully', error:false })
+        } catch (exception) {
+            setNotification({ message: 'wrong credentials', error:true })
+        }
+
     }
 
-    return (
-        <div className="App">
+    // userEffect(()=>{
+    //
+    // },[user])
+
+    const loginForm = () => (
             <form onSubmit={handleLogin}>
                 <div>
                     username <input
@@ -37,8 +53,30 @@ function App() {
                 </div>
                 <button type='submit'>login</button>
             </form>
-        </div>
     )
+
+    const notificationText = () => (
+        <p style={notification.error ? {color: 'red'} : {color: 'green'}}>{notification.message}</p>
+    )
+
+    if ( user ) {
+        return (
+            <div className="App">
+                <h2>blogs</h2>
+                {notificationText()}
+                <p>Hello {user.name}!</p>
+            </div>
+        )
+    } else {
+        return (
+            <div className="App">
+                <h2>log in to application</h2>
+                {notificationText()}
+                {loginForm()}
+            </div>
+        )
+    }
+
 }
 
 export default App
--- src/services/login.js
+++ src/services/login.js
@@ -1,1 +1,11 @@
-//:TODO: login service
+import axios from 'axios'
+const baseUrl = '/api/login'
+
+//:TODO: login service from backend
+const login = async( username, password ) => {
+    const credentials = { 'username':username, 'password':password }
+    const response = await axios.post(baseUrl, credentials)
+    return response.data
+}
+
+export default { login }
